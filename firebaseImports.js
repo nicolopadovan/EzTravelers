ae3f7e192d8dda05b5717925241582c335a9031a
nicolopadovan <89971888+nicolopadovan@users.noreply.github.com>
2022-12-23 20:19:41 +0100

    Add files via upload

---
+++ firebaseImports.js
@@ -0,0 +1,18 @@
+console.log("1. Loading Module");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-functions.js";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAoHPscXLUb52j-PbsakU_TuafJcGNA-3o",
+	authDomain: "eztrippers-lq-mvp.firebaseapp.com",
+	projectId: "eztrippers-lq-mvp",
+	storageBucket: "eztrippers-lq-mvp.appspot.com",
+	messagingSenderId: "1089945914767",
+	appId: "1:1089945914767:web:b6b759161fc88fe2474ffa"
+};
+
+console.log("2. Initializing Firebase");
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app, "europe-west2")
+console.log("3. All initialized");
+const getDestinationsFromAPI = httpsCallable('getDestinationsFromAPI');
