40a07e0e825ee0a07f2bed6bf50254583381e075
Nicolò Padovan <nicolopadovandevelop@gmail.com>
2022-12-26 16:49:17 +0100

    Update interestsHandler.js

--- interestsHandler.js
+++ interestsHandler.js
@@ -60,30 +60,30 @@ nextBtn.addEventListener("click", function () {
 // 	showPrevious();
 // });
 
-saveBtn.addEventListener("click", function () {
+// saveBtn.addEventListener("click", function () {
 
-	const safetyPref = document.getElementById("safetySlider").value;
-	const socialPref = document.getElementById("socialSlider").value;
-	const destinationPref = document.querySelector('input[name="question1"]:checked').value;
-	const objectivePref = document.querySelector('input[name="question2"]:checked').value;
-	const modePref = document.querySelector('input[name="question3"]:checked').value;
+// 	const safetyPref = document.getElementById("safetySlider").value;
+// 	const socialPref = document.getElementById("socialSlider").value;
+// 	const destinationPref = document.querySelector('input[name="question1"]:checked').value;
+// 	const objectivePref = document.querySelector('input[name="question2"]:checked').value;
+// 	const modePref = document.querySelector('input[name="question3"]:checked').value;
 
-	console.log(`Checking ${safetyPref}, ${socialPref}, ${destinationPref}, ${objectivePref}, ${modePref}`);
-	if (!(safetyPref && socialPref && destinationPref && objectivePref && modePref)) {
-		console.log("Check went wrong");
-		return;
-	}
+// 	console.log(`Checking ${safetyPref}, ${socialPref}, ${destinationPref}, ${objectivePref}, ${modePref}`);
+// 	if (!(safetyPref && socialPref && destinationPref && objectivePref && modePref)) {
+// 		console.log("Check went wrong");
+// 		return;
+// 	}
 
-	const interests = {
-		safety: safetyPref,
-		social: socialPref,
-		destination: destinationPref,
-		objective: objectivePref,
-		mode: modePref
-	};
+// 	const interests = {
+// 		safety: safetyPref,
+// 		social: socialPref,
+// 		destination: destinationPref,
+// 		objective: objectivePref,
+// 		mode: modePref
+// 	};
 
-	console.log(currentUser);
-	addDocument(`users/${currentUser.uid}/interests`, "interests", interests).then(() => {
-		window.location.href = "/";
-	});
-})
+// 	console.log(currentUser);
+// 	addDocument(`users/${currentUser.uid}/interests`, "interests", interests).then(() => {
+// 		window.location.href = "/";
+// 	});
+// })
