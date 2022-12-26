52514405146e3a54059233177c8ea877ec296287
Nicolò Padovan <nicolopadovandevelop@gmail.com>
2022-12-26 17:37:24 +0100

    Update interestsHandler.js

--- interestsHandler.js
+++ interestsHandler.js
@@ -19,6 +19,12 @@ function showNext() {
 		return;
 	}
 
+	if (index === structure.length - 1 || index === 0) {
+		nextBtn.style.display = "block";
+	} else {
+		nextBtn.style.display = "none";
+	}
+
 	const currentPage = document.getElementById(structure[index - 1]);
 	const next = document.getElementById(structure[index]);
 
@@ -45,50 +51,52 @@ function showPrevious() {
 	progressBar.style.transform = `scaleX(${(index - 1) / structure.length})`;
 }
 
+function saveData() {
+	const safetyPref = document.getElementById("safetySlider").value;
+	const socialPref = document.getElementById("socialSlider").value;
+	const destinationPref = document.querySelector('input[name="question1"]:checked').value;
+	const objectivePref = document.querySelector('input[name="question2"]:checked').value;
+	const modePref = document.querySelector('input[name="question3"]:checked').value;
+
+	console.log(`Checking ${safetyPref}, ${socialPref}, ${destinationPref}, ${objectivePref}, ${modePref}`);
+	if (!(safetyPref && socialPref && destinationPref && objectivePref && modePref)) {
+		console.log("Check went wrong");
+		return;
+	}
+
+	const interests = {
+		safety: safetyPref,
+		social: socialPref,
+		destination: destinationPref,
+		objective: objectivePref,
+		mode: modePref
+	};
+
+	console.log(currentUser);
+	addDocument(`users/${currentUser.uid}/interests`, "interests", interests).then(() => {
+		window.location.href = "/";
+	});
+}
+
 const radioButtons = document.querySelectorAll("input[type='radio']");
 radioButtons.forEach((radio) => {
 	radio.addEventListener("click", (event) => {
 		radio.parentElement.classList.add("radioAnimation");
 	});
 	radio.parentElement.addEventListener("animationend", () => {
-		radio.parentElement.classList.remove("radioAnimation");
 		showNext();
 	});
 });
 
 
 nextBtn.addEventListener("click", function () {
+	if (index === structure.length - 1) {
+		saveData();
+		return;
+	}
 	showNext();
 });
 
 prevBtn.addEventListener("click", function () {
 	showPrevious();
 });
-
-// saveBtn.addEventListener("click", function () {
-
-// 	const safetyPref = document.getElementById("safetySlider").value;
-// 	const socialPref = document.getElementById("socialSlider").value;
-// 	const destinationPref = document.querySelector('input[name="question1"]:checked').value;
-// 	const objectivePref = document.querySelector('input[name="question2"]:checked').value;
-// 	const modePref = document.querySelector('input[name="question3"]:checked').value;
-
-// 	console.log(`Checking ${safetyPref}, ${socialPref}, ${destinationPref}, ${objectivePref}, ${modePref}`);
-// 	if (!(safetyPref && socialPref && destinationPref && objectivePref && modePref)) {
-// 		console.log("Check went wrong");
-// 		return;
-// 	}
-
-// 	const interests = {
-// 		safety: safetyPref,
-// 		social: socialPref,
-// 		destination: destinationPref,
-// 		objective: objectivePref,
-// 		mode: modePref
-// 	};
-
-// 	console.log(currentUser);
-// 	addDocument(`users/${currentUser.uid}/interests`, "interests", interests).then(() => {
-// 		window.location.href = "/";
-// 	});
-// })
