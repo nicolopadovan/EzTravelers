ca6dd465d385894bff18ce940a97f588d5c9f2f9
Nicolò Padovan <nicolopadovandevelop@gmail.com>
2022-12-27 02:12:46 +0100

    Update searchHandler.js

--- searchHandler.js
+++ searchHandler.js
@@ -1,37 +1,37 @@
-import { getDestinationsFromAPI } from "./firebaseFunctions.js";
-
 const searchBtn = document.getElementById("conversionBtn");
 const searchInput = document.getElementById("searchField");
 searchBtn.addEventListener("click", function (element) {
-	element.preventDefault();
+	import("./firebaseFunctions.js").then(({ getDestinationsFromAPI }) => {
+		element.preventDefault();
 
-	let searchTerm = searchInput.value;
+		let searchTerm = searchInput.value;
 
-	if (searchTerm === '') { return };
+		if (searchTerm === '') { return };
 
-	const filter = document.querySelector('input[name="Radio4"]:checked').value;
-	searchTerm = searchTerm + " " + filter;
+		const filter = document.querySelector('input[name="Radio4"]:checked').value;
+		searchTerm = searchTerm + " " + filter;
 
-	getDestinationsFromAPI({ searchTerm: searchTerm })
-		.then(function (result) {
-			// Read result of the Cloud Function.
-			const destinations = result.data.result
+		getDestinationsFromAPI({ searchTerm: searchTerm })
+			.then(function (result) {
+				// Read result of the Cloud Function.
+				const destinations = result.data.result
 
-			const searchErrorLabel = document.getElementById("searchErrorLabel");
-			if (destinations.length) {
-				searchErrorLabel.style.display = "none";
-				window.location.href = "/destinations/" + destinations[0].slug;
-			} else {
-				searchErrorLabel.style.display = "block";
-			}
-		})
-		.catch(function (error) {
-			console.log(error);
-			const code = error.code;
-			const message = error.message;
-			const details = error.details;
-		});
-})
+				const searchErrorLabel = document.getElementById("searchErrorLabel");
+				if (destinations.length) {
+					searchErrorLabel.style.display = "none";
+					window.location.href = "/destinations/" + destinations[0].slug;
+				} else {
+					searchErrorLabel.style.display = "block";
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+				const code = error.code;
+				const message = error.message;
+				const details = error.details;
+			});
+	});
+});
 
 let searchEventSent = false;
 searchInput.addEventListener("focusout", function (event) {
